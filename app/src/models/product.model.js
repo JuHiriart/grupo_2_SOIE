6d0bfc29f495a95e26ed19dfaefb46be8baf8a45
nachog00 <ignaciog00@gmail.com>
2022-07-24 20:15:30 -0300

    corregi error en products.model

--- app/src/models/product.model.js
+++ app/src/models/product.model.js
@@ -1,20 +1,20 @@
 const {readFileSync,writeFileSync} = require ('fs');
 const { resolve } = require('path');
 
+const fileDir = resolve( __dirname, '..', 'database', 'products.json' );
 
 
-const products = {
-    fileDir : resolve(__dirname, '..','database', 'products.json'),
+
+module.exports = {
     index: () => {
-        let data = readFileSync( this.fileDir );
+        let data = readFileSync( fileDir, 'utf8' );
         return JSON.parse(data);
     },
     add : (product) => {
         let current = this.index();
         current.push(product);
-        let stringified = JSON.stringify(current,{utf8: true})
-        let file = this.fileDir ;
-        writeFileSync( file, stringified );
+        let stringified = JSON.stringify(current,{utf8: true},'\t')
+        writeFileSync( fileDir, stringified );
     },
     getById : ( id ) => {
         let current = this.index();
@@ -23,6 +23,3 @@ const products = {
         } );
     }
 }
-
-module.exports = products ;
-

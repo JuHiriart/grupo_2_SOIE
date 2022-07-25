0e586ba6bc9a21994c343ada5328f37c03a9a371
nachog00 <ignaciog00@gmail.com>
2022-07-25 15:29:05 -0300

    ya funciona el modelo. Carga la vista de 'detail'

--- app/src/models/product.model.js
+++ app/src/models/product.model.js
@@ -1,25 +1,29 @@
 const {readFileSync,writeFileSync} = require ('fs');
 const { resolve } = require('path');
+// const products = require('../controllers/products');
 
 const fileDir = resolve( __dirname, '..', 'database', 'products.json' );
 
 
+const products = {};
 
-module.exports = {
-    index: () => {
-        let data = readFileSync( fileDir, 'utf8' );
-        return JSON.parse(data);
-    },
-    add : (product) => {
-        let current = this.index();
-        current.push(product);
-        let stringified = JSON.stringify(current,{utf8: true},'\t')
-        writeFileSync( fileDir, stringified );
-    },
-    getById : ( id ) => {
-        let current = this.index();
-        current.forEach( (product) => {
-            if (product.id === id) return product;
-        } );
-    }
+products.index = () => {
+    let data = readFileSync( fileDir, 'utf8' );
+    return JSON.parse(data);
+};
+
+products.add = (product) => {
+    let current = products.index();
+    current.push(product);
+    let stringified = JSON.stringify(current,{utf8: true},'\t')
+    writeFileSync( fileDir, stringified );
 }
+
+products.getById = ( id ) => {
+    let current = products.index();
+    current.forEach( (product) => {
+        if (product.id === id) return product;
+    } );
+}
+
+module.exports = products

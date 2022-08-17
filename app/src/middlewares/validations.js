668dc1a6bf0899ce45cece09b70600a424857de4
nachog00 <ignaciog00@gmail.com>
2022-08-17 15:38:28 -0300

    Ya funciona la validacion del registro de usuarios

--- app/src/middlewares/validations.js
+++ app/src/middlewares/validations.js
@@ -1,25 +1,60 @@
 const {check} = require('express-validator');
+const model = require('../models/user.model.js');
 
 module.exports = {
     signIn : [
-        check('first_name').notEmpty().withMessage("Debes ingresar un Nombre").bail(),
-        check('last_name').notEmpty().withMessage("Debes ingresar un Apellido").bail(),
-        // check('email').isEmail().withMessage("Debes ingresar un Email válido").bail(),
-        check('email').custom(value => {
-            return models.users.getByEmail(value).then(user => {
-                if (user) {
-                    return Promise.reject('El email ya existe');
-                }
-            });
-        }).withMessage("El Email ya existe").bail(),
-       // body('password').notEmpty().withMessage("Debes ingresar una contraseña").isLength({min:4, max:16}).withMessage("la contraseña debe tener entre 4 y 16 caracteres").custom(async (passCon, {req}) => { 
-       // const password = req.body.password;
-    //
-       //     if(password !== passCon){
-       //         throw new Error('las contraseñas deben ser iguales')
-       //     }
-       // }),
-       // body('passCon').notEmpty().withMessage("Debes confirmar la contraseña")
-    ],
+
+        check('first_name')
+        
+            .notEmpty()
+            .withMessage("Debes ingresar un nombre")
+            .bail(),
+
+        check('last_name')
+
+            .notEmpty()
+            .withMessage("Debes ingresar un apellido")
+            .bail(),
+
+        check('email')
+
+            .notEmpty()
+            .withMessage("Debes ingresar un email")
+            .bail()
+        
+            .isEmail()
+            .withMessage("Debes ingresar un email válido")
+            .bail()
+        
+            .custom( value => { // chequeo que el email no exista en la base de datos
+                return model.getByEmail(value) ? false : true; // si el email ya existe, devuelve false
+            }).withMessage("El Email ya existe")
+            .bail(),
+
+        check('password')
+        
+            .notEmpty()
+            .withMessage("Debes ingresar una contraseña")
+            .bail()
+            
+            .isLength({min:4, max:16})
+            .withMessage("la contraseña debe tener entre 4 y 16 caracteres")
+            .bail()
+
+            .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{0,}$/) // al menos un numero, una mayuscula y una minuscula
+            .withMessage("La contraseña debe tener al menos una mayúscula, una minúscula y un número"),
+            
+        check('passCon')
+
+            .notEmpty()
+            .withMessage("Debes confirmar la contraseña")
+            .bail()
+
+            .custom(async (passCon, {req}) => { 
+                const password = req.body.password;
+                return password !== passCon ; 
+            })
+            .withMessage("Las contraseñas no coinciden")
+        ],
     signUp : [],
 }

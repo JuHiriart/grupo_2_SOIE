1fd58d2fad3bd726275daced6e3c318525356a0b
MARIO A PULIDO SUAREZ <66319905+mpulidosuarez73@users.noreply.github.com>
2022-08-27 22:57:35 -0500

    formato

--- app/src/database/models/User.js
+++ app/src/database/models/User.js
@@ -1,60 +1,61 @@
-module.export =(sequelize, dataTypes) => {
-let alias="Users";
-let cols={
-    id:{
-        type: dataTypes.INTEGER,
-        primaryKey:true,
-        autoIncrement:true
+module.export = (sequelize, dataTypes) => {
+    let alias = "Users";
+    let cols = {
+        id: {
+            type: dataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
 
 
-    },
-    firstName:{
-        type: dataTypes.STRING,
-        allowNull: false
+        },
+        firstName: {
+            type: dataTypes.STRING,
+            allowNull: false
 
 
-    },
-    lastName: {
-        type:dataTypes.STRING,
-        allowNull: false
+        },
+        lastName: {
+            type: dataTypes.STRING,
+            allowNull: false
 
-    },
-    birth: {
-        type:dataTypes.DATE,
-        allowNull: false
+        },
+        birth: {
+            type: dataTypes.DATE,
+            allowNull: false
 
-    },
-    email: {
-        type:dataTypes.STRING,
+        },
+        email: {
+            type: dataTypes.STRING,
 
-    },
-    password: {
-        type:dataTypes.STRING,
-    },
-    idCategory: {
-        type: DataTypes.INTEGER
+        },
+        password: {
+            type: dataTypes.STRING,
+        },
+        idCategory: {
+            type: DataTypes.INTEGER
 
-    },
-    image: {
-        type:dataTypes.STRING,
+        },
+        image: {
+            type: dataTypes.STRING,
 
-    },
-    adress: {
-        type:dataTypes.STRING,
+        },
+        adress: {
+            type: dataTypes.STRING,
 
-    },
-    numberPhone: {
-        type:dataTypes.STRING,
+        },
+        numberPhone: {
+            type: dataTypes.STRING,
 
-    },
+        },
 
-};
+    };
 
-let config = {tableName:"Users",
-    timesTamps:false
-};
+    let config = {
+        tableName: "Users",
+        timesTamps: false
+    };
 
-    const User = sequelize.define(alias,cols,config);
+    const User = sequelize.define(alias, cols, config);
 
     return User;
 };
